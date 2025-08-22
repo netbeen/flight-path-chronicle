f16c33fed588239a9fce1f6081a4d6caa0392d6a
Yang Yang <yangyang.jeff@bytedance.com>
2025-08-23 00:50:45 +0800

    feat(data): 更新机场和航班数据
    - 简化机场名称，移除冗余的"国际机场"字样
    - 新增多个国内外机场数据
    - 更新航班数据，增加更多国际航线
    - 调整航班时间覆盖更广的时间范围

--- src/data/airport.ts
+++ src/data/airport.ts
@@ -12,40 +12,70 @@ export interface Airport {
 /** 预定义的机场数据 */
 export const AIRPORTS: Airport[] = [
   {
-    name: '北京首都国际机场',
+    name: '北京首都',
     code: 'PEK',
     latitude: 40.0724,
     longitude: 116.5971,
   },
   {
-    name: '上海浦东国际机场',
+    name: '上海浦东',
     code: 'PVG',
     latitude: 31.1559,
     longitude: 121.8053,
   },
   {
-    name: '上海虹桥国际机场',
+    name: '上海虹桥',
     code: 'SHA',
     latitude: 31.1959,
     longitude: 121.3417,
   },
   {
-    name: '广州白云国际机场',
+    name: '广州白云',
     code: 'CAN',
     latitude: 23.3896,
     longitude: 113.3057,
   },
   {
-    name: '成都双流国际机场',
-    code: 'CTU',
-    latitude: 30.5785,
-    longitude: 103.9471,
+    name: '北京大兴',
+    code: 'PKX',
+    latitude: 39.509945,
+    longitude: 116.41092,
   },
   {
-    name: '西安咸阳国际机场',
-    code: 'XIY',
-    latitude: 34.4388,
-    longitude: 108.7583,
+    name: '杭州萧山',
+    code: 'HGH',
+    latitude: 30.234345,
+    longitude: 120.437058,
+  },
+  {
+    name: '新加坡樟宜',
+    code: 'SIN',
+    latitude: 1.3644,
+    longitude: 103.9915,
+  },
+  {
+    name: '香港赤鱲角',
+    code: 'HKG',
+    latitude: 22.3080,
+    longitude: 113.9185,
+  },
+  {
+    name: '西雅图塔科马',
+    code: 'SEA',
+    latitude: 47.4502,
+    longitude: -122.3088,
+  },
+  {
+    name: '厦门高崎',
+    code: 'XMN',
+    latitude: 24.5443,
+    longitude: 118.128,
+  },
+  {
+    name: '普吉岛',
+    code: 'HKT',
+    latitude: 8.113,
+    longitude: 98.317,
   },
 ];
 
